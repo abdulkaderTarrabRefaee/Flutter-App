45a0d185918d7f9f136a1b0ac1517bd336f968cf
User <User@DESKTOP-2VVV9T8>
2021-04-19 18:07:16 +0300

    Revert " dropdown provider city  and   diseas and  dataSearch comes and view  in screen with synchronization with detal view with type with page button with galury2 WITH USER MODEL with splash and  lang select image"
    This reverts commit 1c5582eee111b04601ea42e2c751765e322be44f.with stff

--- lib/view/detalis_hos.dart
+++ lib/view/detalis_hos.dart
@@ -10,10 +10,10 @@ import 'package:gpluseclinicapp/view/appointment_view.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_html/style.dart';
 import 'package:gpluseclinicapp/view/detalis_dr.dart';
-import 'package:photo_view/photo_view.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 import 'package:cached_network_image/cached_network_image.dart';
+
 class DetailedProfileHos extends StatefulWidget {
   HospitalDoctorClinic hospitalDoctorClinic;
   List<bool> typeSelected;
@@ -32,7 +32,7 @@ class _DetailedProfileHosState extends State<DetailedProfileHos> {
   int maxNumberPage;
   int maxNumberResult;
   List<bool> pageSelected;
-  int parntid=1;
+  int parntid ;
   void incPage() {
     if (pageSelected[0] == true && pageNumberInc > 1) {
       pageNumberInc--;
@@ -41,9 +41,17 @@ class _DetailedProfileHosState extends State<DetailedProfileHos> {
       pageNumberInc++;
     }
   }
-  @override
-  void initState() {
 
+  @override
+  // ignore: must_call_super
+  void initState() {
+    Stream.fromFuture(GplusApi().fetchProfileHosData(
+            widget.hospitalDoctorClinic, widget.hospitalDoctorClinic.type))
+        .listen((event) {
+      parntid=event.profile.datas.first.parentId;
+      print(parntid);
+      return (event.profile.datas.first.parentId);
+    });
   }
 
   @override
@@ -54,7 +62,18 @@ class _DetailedProfileHosState extends State<DetailedProfileHos> {
         appBar: AppBar(
           bottom: TabBar(
             labelColor: Colors.black12,
-            onTap: (index) {},
+            onTap: (index) {
+              if(index==2)
+                setState(() {
+                  Stream.fromFuture(GplusApi().fetchProfileHosData(
+                      widget.hospitalDoctorClinic, widget.hospitalDoctorClinic.type))
+                      .listen((event) {
+                    parntid=event.profile.datas.first.parentId;
+                    print(parntid);
+                    return (event.profile.datas.first.parentId);
+                  });
+                });
+            },
             tabs: [
               Tab(icon: Icon(Icons.local_hospital)),
               Tab(icon: Icon(Icons.image)),
@@ -62,7 +81,7 @@ class _DetailedProfileHosState extends State<DetailedProfileHos> {
               Tab(icon: Icon(Icons.person)),
             ],
           ),
-          title: Text("snapshot.data.profile.data.name"),
+          title: Text("Hospital Data"),
         ),
         body: TabBarView(children: [
           FutureBuilder<ProfileHos>(
@@ -71,7 +90,7 @@ class _DetailedProfileHosState extends State<DetailedProfileHos> {
                   widget.hospitalDoctorClinic.type),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  parntid=snapshot.data.profile.data.parentId;
+                  parntid = snapshot.data.profile.data.parentId;
                   initState();
                   print(parntid);
                   return SafeArea(
@@ -294,34 +313,33 @@ class _DetailedProfileHosState extends State<DetailedProfileHos> {
                   widget.hospitalDoctorClinic,
                   widget.hospitalDoctorClinic.type),
               builder: (context, snapshot) {
-
                 if (snapshot.hasData) {
-
-                  return  GridView.count(
-                      crossAxisCount: 3,
-                      children: List.generate(snapshot.data.gallery.datas.length, (index) {
-                        GalleryData  asset = snapshot.data.gallery.datas[index];
-                     return
-                       Padding(
-                         padding: const EdgeInsets.all(8.0),
-                         child: CachedNetworkImage(
-                           imageUrl: urlImage+asset.image,
-                           imageBuilder: (context, imageProvider) => Container(
-                             decoration: BoxDecoration(
-                               image: DecorationImage(
-                                   image: imageProvider,
-                                   fit: BoxFit.cover,),
-                             ),
-                           ),
-                           placeholder: (context, url) => Padding(
-                             padding: const EdgeInsets.all(18.0),
-                             child: CircularProgressIndicator(),
-                           ),
-                           errorWidget: (context, url, error) => Icon(Icons.error),
-                         ),
-                       );
-                      }),
-
+                  return GridView.count(
+                    crossAxisCount: 3,
+                    children: List.generate(snapshot.data.gallery.datas.length,
+                        (index) {
+                      GalleryData asset = snapshot.data.gallery.datas[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CachedNetworkImage(
+                          imageUrl: urlImage + asset.image,
+                          imageBuilder: (context, imageProvider) => Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: imageProvider,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          placeholder: (context, url) => Padding(
+                            padding: const EdgeInsets.all(18.0),
+                            child: CircularProgressIndicator(),
+                          ),
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                        ),
+                      );
+                    }),
                   );
                 } else
                   return SafeArea(
@@ -333,32 +351,50 @@ class _DetailedProfileHosState extends State<DetailedProfileHos> {
                     ),
                   );
               }),
-
           FutureBuilder<Staff>(
+              future: GplusApi().fetchStaffData(parntid, 2, 1, ""),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
 
-        future: GplusApi().fetchStaffData(parntid, 2, 1, "search"),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var maxNumberPage = snapshot.data.totalPage;
-            var pageCurrentNumber = snapshot.data.pageNumber;
-            return Text(snapshot.data.staffData.length.toString());
-          } else
-            return SafeArea(
-              child: Container(
-                color: Colors.white,
-                child: Center(
-                  child: CircularProgressIndicator(backgroundColor: Colors.red),
-                ),
-              ),
-            );
-        }),
+                  var maxNumberPage = snapshot.data.totalPage;
+                  var pageCurrentNumber = snapshot.data.pageNumber;
+                  return Column(
+                    children: [
+                      Text("data"),
+                      Expanded(
 
+                        child: ListView.builder(
+                            padding: const EdgeInsets.all(10),
+                            scrollDirection: Axis.horizontal,
+                            itemCount: snapshot.data.staffData.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return cardItem(
+                                  context,
+                                  snapshot.data.staffData.toList()[index],
+                                  widget.typeSelected);
+                            }),
+                      ),
+                      Text("sd")
+                    ],
+                  );
+                } else
+                  return SafeArea(
+                    child: Container(
+                      color: Colors.white,
+                      child: Center(
+                        child: CircularProgressIndicator(
+                            backgroundColor: Colors.red),
+                      ),
+                    ),
+                  );
+              }),
           Text("dd"),
         ]),
       ),
     );
   }
 }
+
 class ImageDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -368,16 +404,13 @@ class ImageDialog extends StatelessWidget {
         height: 200,
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: ExactAssetImage('assets/tamas.jpg'),
-                fit: BoxFit.cover
-            )
-        ),
+                image: ExactAssetImage('assets/tamas.jpg'), fit: BoxFit.cover)),
       ),
     );
   }
 }
 
-Widget _getImage(url ) {
+Widget _getImage(url) {
   final imageProvider = ResizeImage.resizeIfNeeded(
     200,
     250,
@@ -390,19 +423,17 @@ Widget _getImage(url ) {
     image: imageProvider,
   );
 
-
-
   return image;
 }
 
-Widget cardItem(BuildContext context, StaffData staff,
-    List<bool> typeSelected) {
+Widget cardItem(
+    BuildContext context, StaffData staff, List<bool> typeSelected) {
   String urlImage = "https://gplusclinic.com/images/upload/";
-  return  Row(
+  return Row(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
       InkWell(
-        hoverColor:Colors.red,
+        hoverColor: Colors.red,
         child: Container(
           height: 250,
           margin: EdgeInsets.only(top: 30),
@@ -432,11 +463,13 @@ Widget cardItem(BuildContext context, StaffData staff,
                       children: [
                         Container(
                           width: 140,
-                          margin: EdgeInsets.only(left: 15, right: 15,),
+                          margin: EdgeInsets.only(
+                            left: 15,
+                            right: 15,
+                          ),
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(10),
-                            child: _getImage(
-                                urlImage + staff.image),
+                            child: _getImage(urlImage + staff.image),
                           ),
                         ),
                         if (staff.isFreeConsultation)
@@ -458,7 +491,7 @@ Widget cardItem(BuildContext context, StaffData staff,
                     ),
                     if (staff.isAward)
                       Transform.rotate(
-                        angle: -22/7 / 6,
+                        angle: -22 / 7 / 6,
                         child: Container(
                           width: 60,
                           child: Image.network(
@@ -470,7 +503,6 @@ Widget cardItem(BuildContext context, StaffData staff,
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-
                 children: [
                   new Divider(
                     indent: 16,
@@ -494,7 +526,6 @@ Widget cardItem(BuildContext context, StaffData staff,
                       Text(staff.rate.toString(),
                           style: TextStyle(fontSize: 7)),
                       Container(
-
                         child: RatingBar.builder(
                           ignoreGestures: true,
                           initialRating: staff.rate.toDouble(),
@@ -510,17 +541,17 @@ Widget cardItem(BuildContext context, StaffData staff,
                       ),
                     ],
                   ),
-                  Text("from "+staff.rateCount.toString()+ "  verified reviews ",style: TextStyle(fontSize: 6)),
-
+                  Text(
+                      "from " +
+                          staff.rateCount.toString() +
+                          "  verified reviews ",
+                      style: TextStyle(fontSize: 6)),
                   Container(
                     width: 180,
                     height: 100,
                     margin: EdgeInsets.only(top: 10, bottom: 10),
                     alignment: Alignment.topLeft,
-                    child: Text(
-                        staff.summary == null
-                            ? ""
-                            : staff.summary,
+                    child: Text(staff.summary == null ? "" : staff.summary,
                         style: TextStyle(fontSize: 10)),
                   ),
                 ],
@@ -531,15 +562,12 @@ Widget cardItem(BuildContext context, StaffData staff,
         onTap: () {
           print(staff.name);
           print(typeSelected);
-
         },
-
-
       ),
       Container(
         height: 100,
         width: 10,
-        padding: EdgeInsets.only(bottom: 10,top: 10,left: 10,right: 10),
+        padding: EdgeInsets.only(bottom: 10, top: 10, left: 10, right: 10),
       ),
     ],
 
@@ -553,14 +581,12 @@ void _showResultSearchDoc(BuildContext context,
   Navigator.push(context, MaterialPageRoute(builder: (_) {
     return new DetailedProfileDoc(hospitalDoctorClinics, typeSelected);
   }));
-
-
 }
+
 void _showResultSearchHos(BuildContext context,
     HospitalDoctorClinic hospitalDoctorClinics, typeSelected) {
   print(typeSelected);
   Navigator.push(context, MaterialPageRoute(builder: (_) {
     return new DetailedProfileHos(hospitalDoctorClinics, typeSelected);
-
   }));
 }
